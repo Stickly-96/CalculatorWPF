ad4d2b4862c56161cc9c12230136a9fd5c95527c
Vladimir <Grib.vladimir-96@yandex.ru>
2021-01-17 19:01:14 +0300

    third commit

--- Calc/MainWindow.xaml.cs
+++ Calc/MainWindow.xaml.cs
@@ -82,7 +82,9 @@ namespace Calc
                     break;
                 case "avg":
                     result = (num1 + num2) / 2;
-
+                    break;
+                case "x^y":
+                    result = Pow(num1, num2);
                     break;
 
             }
@@ -90,6 +92,81 @@ namespace Calc
             op = "";
             num1 = result;
         }
+
+        // x^4 = x * x * x * x * x^3 *x;
+        // x^3 = x * x * x * x^2 * x;
+        // x^2 = x * x * x^1 * x;
+        // x^1 = x * x*0 * x;
+        // x^0 = 1;
+
+        private int Pow(int x, int y)
+        {
+            if (y == 0)
+                return 1;
+
+            return Pow(x, y - 1) * x;
+        }
+
+        private void btn_C_Click(object sender, RoutedEventArgs e)
+        {
+            num1 = 0;
+            num2 = 0;
+            op = "";
+            txtValue.Text = "0";
+
+        }
+
+        private void btn_CE_Click(object sender, RoutedEventArgs e)
+        {
+            if (op == "")
+            {
+                num1 = 0;
+            }
+            else
+            {
+                num2 = 0;
+            }
+            txtValue.Text = "0";
+        }
+
+        private void btn_backspace_Click(object sender, RoutedEventArgs e)
+        {
+            if (op == "0")
+            {
+                num1 = num1 / 10;
+                txtValue.Text = num1.ToString();
+            }
+            else
+            {
+                num2 = num2 / 10;
+                txtValue.Text = num2.ToString();
+            }
+        }
+
+        private void btn_plusminus_Click(object sender, RoutedEventArgs e)
+        {
+            if (op == "")
+            {
+                num1 *= -1;
+                txtValue.Text = num1.ToString();
+            }
+            else
+            {
+                num2 *= num2;
+                txtValue.Text = num2.ToString();
+            }
+        }
+
+
+        //private int Pow(int x, int y)
+        //{
+        //    int result = 1;
+        //    for(int i = 1; i <= y; i++)
+        //    {
+        //        result *= x;
+        //    }
+        //    return result;
+        //}   
 
     }
 }
